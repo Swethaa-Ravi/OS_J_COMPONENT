381acba62bf958b117ba071d33d5f185a03342b4
Swethaa-Ravi <64682109+Swethaa-Ravi@users.noreply.github.com>
2020-10-25 13:45:36 -0400

    Add files via upload

---
+++ uapi_mm.h
@@ -0,0 +1,21 @@
+#ifndef __UPAI_MM__
+#define __UAPI_MM__
+
+#include <stdint.h>
+
+/* Initialization function */
+
+void
+mm_init();
+
+/* Registration function*/
+
+void
+mm_instantiate_new_page_family(
+               char *struct_name,
+               uint32_t struct_size);
+
+#define MM_REG_STRUCT(struct_name)   \
+        (mm_instantiate_new_page_family(#struct_name, sizeof(struct_name)))
+
+#endif /* __UAPI_MM__ */
